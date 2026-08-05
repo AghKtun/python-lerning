57d275b9f19f93551d83b42c44934a1a90dd95d7
AghKtun <faffneer@gmail.com>
2026-08-05 14:28:56 +0900

    Create 08 Min-max digit.py

---
+++ chapter_5: Data type/theme_1: int, float, max, min, abs/08 Min-max digit.py
@@ -0,0 +1,13 @@
+# Min-max digit
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+
+minimalnoe = min(a, b, c, d, e)
+maximalnoe = max(a, b, c, d, e)
+
+print('Наименьшее число =', minimalnoe)
+print('Наибольшее число =', maximalnoe)
