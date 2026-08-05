85b64e870c3af6bea4e8f9bf430dfd86d3883396
AghKtun <faffneer@gmail.com>
2026-08-05 14:30:24 +0900

    Create 09 Absolute sum.py

---
+++ chapter_5: Data type/theme_1: int, float, max, min, abs/09 Absolute sum.py
@@ -0,0 +1,11 @@
+# Absolute sum
+
+a_1 = float(input())
+a_2 = float(input())
+a_3 = float(input())
+a_4 = float(input())
+a_5 = float(input())
+
+summa = abs(a_1) + abs(a_2) + abs(a_3) + abs(a_4) + abs(a_5)
+
+print(summa)
