e8e59e204c51fc0cacffed32935430972bacfbe4
AghKtun <faffneer@gmail.com>
2026-08-06 16:23:48 +0900

    Create 01 Number of numbers.py

---
+++ chapter_6: for, while/theme_3-4: Counters, variable swapping, extended operators, flags/01 Number of numbers.py
@@ -0,0 +1,12 @@
+# Number of numbers
+
+a = int(input())
+b = int(input())
+
+counter = 0
+
+for i in range(a, b + 1):
+    if pow(i, 3) % 10 == 4 or pow(i, 3) % 10 == 9:
+        counter += 1
+        
+print(counter)
